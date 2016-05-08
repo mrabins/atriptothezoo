76b19c4ea08696a69866fd0f685dd0ce22df9d11
Mark Rabins <markrabins@gmail.com>
2016-05-08 10:55:12 -0700

    Add images, create a penguin instance

---
+++ A Trip to the Zoo/Penguin.swift
@@ -0,0 +1,17 @@
+//
+//  Penguin.swift
+//  A Trip to the Zoo
+//
+//  Created by Mark Rabins on 5/8/16.
+//  Copyright © 2016 self.edu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Penguin {
+    var age: Int
+    var name: String
+    var species: String
+    var image: UIImage
+}
