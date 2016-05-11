4f9ba0a290e574d066664e19f9013c42e97101f3
Mark Rabins <markrabins@gmail.com>
2016-05-10 23:36:49 -0700

    Uncomment some code

--- A Trip to the Zoo/ViewController.swift
+++ A Trip to the Zoo/ViewController.swift
@@ -35,6 +35,7 @@ class ViewController: UIViewController {
         
         let ageOfPenguinInHumanYears = myPenguin.ageInPenguinYearsFromHumanYears()
         print(ageOfPenguinInHumanYears)
+        
 
     }
 
